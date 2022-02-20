71b54f4d30955678c01e24e2a6768d2a8ba55ec4
Welcome <Welcome@DESKTOP-U43JIC9>
2022-02-20 15:57:08 +0530

    Add project files.

---
+++ largest value find/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace largest_value_find
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number:");
+            int a=int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            int b=int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter third number:");
+            int c = int.Parse(Console.ReadLine());
+            {
+                if(a>b && a > c)
+                {
+                    Console.WriteLine("the larset number is=" + a);
+
+                }
+                else if(b>c && b > a)
+                {
+                    Console.WriteLine("the larset number is=" + b);
+
+                }
+                else if(c>a && c>b)
+                {
+                    Console.WriteLine("the larset number is=" + c);
+
+                }
+            }
+
+        }
+    }
+}
